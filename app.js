3447ae6a89b384fcae7e1aafcf55da91b30bf7de
bad fad <badfad@edu.novia.fi>
2023-02-08 07:02:18 +0200

    eslint config 3

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-/* eslint-disable no-undef */
+
 // eslint-disable-next-line no-undef
 const express = require('express')
 const app = express()
 
 // Heroku dynamically sets a port
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-    console.log('server started on port 5000')
+  console.log('server started on port 5000')
 })
